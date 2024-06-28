f387905a8b10d877dc9e7d077f311290504df215
ooferdoodles <pantaisuyasri@gmail.com>
2024-06-28 11:07:50 +0700

    lkfddsfn

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from tkinter import *
 from tkinter import filedialog
 from skimage.metrics import structural_similarity
 import threading
+import queue
+import time
 
 
 class PCBQualityAssuranceApp:
@@ -21,10 +23,13 @@ class PCBQualityAssuranceApp:
 
         self.reference_image = None
         self.current_frame = None
+        self.processed_frame = None
+        self.frame_queue = queue.Queue(maxsize=1)  # Queue to hold frames for processing
+        self.flicker_state = True
 
-        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Changed to use default camera
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        self.cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)  # Changed to use default camera
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
         print("Initialized Webcam")
 
         # Set up the GUI
@@ -36,6 +41,11 @@ class PCBQualityAssuranceApp:
         self.capture_thread.daemon = True
         self.capture_thread.start()
 
+        # Start the image processing thread
+        self.processing_thread = threading.Thread(target=self.process_output)
+        self.processing_thread.daemon = True
+        self.processing_thread.start()
+
         # Release the video capture when the app closes
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
@@ -43,7 +53,7 @@ class PCBQualityAssuranceApp:
         # Configure grid layout
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
-        self.root.grid_columnconfigure(1, weight=2)
+        self.root.grid_columnconfigure(1, weight=3)
 
         # Create frames
         self.left_frame = Frame(self.root, bg="azure2")
@@ -54,9 +64,11 @@ class PCBQualityAssuranceApp:
         self.right_frame.grid(row=0, column=1, sticky="nswe")
         self.right_frame.pack_propagate(False)
 
-        # Left Frame
+        # ---------------------------------------------------------------------------- #
+        #                                  Left Frame                                  #
+        # ---------------------------------------------------------------------------- #
 
-        ## Input Frame
+        # ---------------------------------- Cameras --------------------------------- #
         self.input_label = Label(self.left_frame, text="Input Image", bg="azure1")
         self.input_label.pack(fill="x", padx=5, pady=(5, 0))
         self.input_canvas = Canvas(self.left_frame)
@@ -69,7 +81,7 @@ class PCBQualityAssuranceApp:
         self.reference_canvas = Canvas(self.left_frame)
         self.reference_canvas.pack(fill="x", padx=5, pady=(5, 0))
 
-        ## Button Frame
+        # ---------------------------------- Buttons --------------------------------- #
         self.button_frame = Frame(self.left_frame)
         self.button_frame.pack(fill="x", padx=5, pady=(5, 0))
 
@@ -97,7 +109,7 @@ class PCBQualityAssuranceApp:
         )
         self.upload_file_button.pack(side=LEFT, expand=True, fill="both")
 
-        ## Mode Frame
+        # ---------------------------- Mode Radio Buttons ---------------------------- #
         self.mode_label = Label(self.left_frame, text="Output Mode", bg="azure1")
         self.mode_label.pack(fill="x", padx=5, pady=(5, 0))
         self.mode = StringVar(value="none")
@@ -106,6 +118,7 @@ class PCBQualityAssuranceApp:
             ("Overlay", "overlay"),
             ("Difference", "difference"),
             ("SSIM", "ssim"),
+            ("Flicker", "flicker"),
         )
 
         for mode_text, mode_value in modes:
@@ -118,7 +131,9 @@ class PCBQualityAssuranceApp:
             )
             r.pack(fill="x", padx=5)
 
-        # Right Frame
+        # ---------------------------------------------------------------------------- #
+        #                                  Right Frame                                 #
+        # ---------------------------------------------------------------------------- #
         self.output_label = Label(self.right_frame, text="Output Image", bg="azure1")
         self.output_label.pack(fill="x", padx=5, pady=(5, 0))
         self.output_canvas = Canvas(self.right_frame, bg="azure1")
@@ -135,6 +150,15 @@ class PCBQualityAssuranceApp:
             ret, frame = self.cap.read()
             if ret:
                 self.current_frame = frame
+                # Only put the latest frame into the queue, discard the old one if the queue is full
+                if not self.frame_queue.full():
+                    self.frame_queue.put(frame)
+                else:
+                    try:
+                        self.frame_queue.get_nowait()
+                    except queue.Empty:
+                        pass
+                    self.frame_queue.put(frame)
 
     def resize_all_canvases(self, event):
         def resize_canvas(canvas, frame):
@@ -148,6 +172,8 @@ class PCBQualityAssuranceApp:
         resize_canvas(self.output_canvas, self.right_frame)
         resize_canvas(self.input_canvas, self.left_frame)
         resize_canvas(self.reference_canvas, self.left_frame)
+
+    # ------------------------- Display Update Functions ------------------------- #
 
     def update_display(self):
         if self.current_frame is not None:
@@ -193,67 +219,87 @@ class PCBQualityAssuranceApp:
         canvas_width = self.output_canvas.winfo_width()
         canvas_height = self.output_canvas.winfo_height()
         target_size = (canvas_width, canvas_height)
-        if self.reference_image is None:
+        if self.processed_frame is None:
             self.output_canvas.create_image(0, 0, anchor=NW, image=None)
             self.output_canvas.image = None
             return
 
-        frame = self.current_frame
-        mode = self.mode.get()
-        try:
-            if mode == "overlay":
-                output = self.process_overlay(frame, self.reference_image)
-            elif mode == "difference":
-                output = self.process_difference(frame, self.reference_image)
-            elif mode == "ssim":
-                output = self.process_ssim(frame, self.reference_image)
-            else:
-                output = frame
-        except Exception as e:
-            print(f"Error processing image: {e}")
-
-        converted_frame = self.convert_frame_to_photoimage(output, target_size)
+        converted_frame = self.convert_frame_to_photoimage(
+            self.processed_frame, target_size
+        )
         self.output_canvas.create_image(0, 0, anchor=NW, image=converted_frame)
         self.output_canvas.image = converted_frame
 
-    def process_overlay(self, input, reference, transparency=0.5):
-        return cv2.addWeighted(input, transparency, reference, transparency, 0)
+    # ------------------------- Image Processing Functions ------------------------- #
 
-    def process_difference(self, input, reference):
-        return 255 - cv2.absdiff(input, reference)
+    def process_output(self):
+        while True:
+            frame = self.frame_queue.get()  # Wait for a frame to be available
+            if frame is not None:
+                try:
+                    processed_frame = self.process_current_frame(frame)
+                    self.processed_frame = processed_frame
+                except Exception as e:
+                    print(f"Error processing output frame: {e}")
 
-    def process_ssim(self, input, reference):
-        _, diff = structural_similarity(
-            cv2.cvtColor(input, cv2.COLOR_BGR2GRAY),
-            cv2.cvtColor(reference, cv2.COLOR_BGR2GRAY),
-            full=True,
-        )
+    def process_current_frame(self, frame):
+        mode = self.mode.get()
+        if mode == "overlay":
+            output = self.overlay_images(self.reference_image, frame)
+        elif mode == "difference":
+            output = self.difference_image(self.reference_image, frame)
+        elif mode == "ssim":
+            output = self.ssim_image(self.reference_image, frame)
+        elif mode == "flicker":
+            output = self.flicker_image(self.reference_image, frame)
+        else:
+            output = frame
+
+        return output
+
+    def overlay_images(self, reference_image, current_frame):
+        alpha = 0.5
+        return cv2.addWeighted(reference_image, alpha, current_frame, 1 - alpha, 0)
+
+    def difference_image(self, reference_image, current_frame):
+        return 255 - cv2.absdiff(reference_image, current_frame)
+
+    def ssim_image(self, reference_image, current_frame):
+        gray_reference = cv2.cvtColor(reference_image, cv2.COLOR_BGR2GRAY)
+        gray_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        _, diff = structural_similarity(gray_reference, gray_frame, full=True)
         diff = (diff * 255).astype("uint8")
-        return diff
+        diff_color = cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR)
+        return diff_color
+
+    def flicker_image(self, reference_image, frame, delay=0.1):
+        time.sleep(delay)
+        self.flicker_state = not self.flicker_state
+        return reference_image if self.flicker_state else frame
+
+    # ----------------------------- Button Functions ----------------------------- #
 
     def capture_reference(self):
-        if self.current_frame is not None:
-            self.reference_image = self.current_frame
-            print("Reference image captured")
+        self.reference_image = self.current_frame.copy()
 
     def clear_reference(self):
         self.reference_image = None
 
     def upload_reference(self):
-        file_path = filedialog.askopenfilename(
-            filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp;*.tiff")]
-        )
+        file_path = filedialog.askopenfilename()
         if file_path:
             self.reference_image = cv2.imread(file_path)
-            print("Reference image uploaded")
-            self.update_reference_display()
+
+    # ------------------------------ Other Functions ----------------------------- #
 
     def convert_frame_to_photoimage(self, frame, target_size):
+        if frame is None:
+            return None
+
         resized_frame = cv2.resize(frame, target_size)
-        converted_frame = ImageTk.PhotoImage(
-            image=PILImage.fromarray(cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB))
-        )
-        return converted_frame
+        rgb_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
+        pil_image = PILImage.fromarray(rgb_frame)
+        return ImageTk.PhotoImage(image=pil_image)
 
     def on_closing(self):
         self.cap.release()
